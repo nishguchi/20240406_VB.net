cac2787b57e6c2deadd6e035a807f60e7936f836
NISHIGUCHI.K <112517270+nishguchi@users.noreply.github.com>
2024-04-08 00:19:40 +0900

    Add files via upload
    Format change

--- DateForm.vb
+++ DateForm.vb
@@ -1,3 +1,11 @@
 ﻿Public Class DateForm
+    Private Sub DateForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim dateTime As DateTime = DateTime.Now
+        'Label1.Text = dateTime.ToString.Format("yyyy/MM/dd HH:mm:ss") 変換されない
+        Label1.Text = Format(dateTime, "yyyy年MM月dd日 HH時 mm分 ss秒")
+
+    End Sub
+
 
 End Class
